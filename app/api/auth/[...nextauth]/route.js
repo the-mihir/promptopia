0b5a662a3b645a56cdb53b179b87098c68d86377
Mihir <mihirkantho@gmail.com>
2024-08-19 23:28:20 +0600

    adding dot nev

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 import User from "@/models/user";
 import { connectToDB } from "@/utils/database";
+require('dotenv').config()
 
 const handler = NextAuth({
     providers: [
--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 let isConnected = false;
 
