ae2d3a7a6c2971bc6ebd4626d8b6bc4461f7f1a2
Mihir <mihirkantho@gmail.com>
2024-08-20 00:08:17 +0600

    change database file

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,6 @@
 import mongoose from 'mongoose';
 
-
 let isConnected = false;
-
-
 
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
@@ -14,17 +11,22 @@ export const connectToDB = async () => {
     }
 
     try {
+        const mongoUri = process.env.MONGODB_URI;
 
+        if (!mongoUri) {
+            throw new Error("MONGODB_URI is not defined in environment variables.");
+        }
 
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(mongoUri, {
             dbName: "share_prompt",
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
 
         isConnected = true;
-        console.log('MongoDB Connected');
-    } catch (e) {
-        console.log('MongoDB connection error:', e);
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        throw error; // Optional: Rethrow the error if you want to handle it higher up in the call stack.
     }
 };
